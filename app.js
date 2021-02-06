9359050cb871c20f5ef22c71b442a3577ba1a868
Lingyv77 <Lingyv77@163.com>
2021-02-06 12:34:43 +0800

    04

--- app.js
+++ app.js
@@ -1,39 +1,62 @@
+const TOKEN = 'token';
 // app.js
 App({
+  globalData: {
+    token: null
+  },
   onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
+    //1.先从缓存中取出token
+    const token = wx.getStorageSync(TOKEN);
+    
+    //2.判断token是否有值
+    if(token && token.length !== 0) { //token已经有值, 验证token是否过期
+      this.toeck_token(token)  //验证token是否过期
+    }else { //没有值进行登录操作
+      this.login();
+    }
+  },
+  toeck_token(token) {
+    console.log('有缓存记录');
+    wx.request({
+      url: 'http://httpbin.org',
+      hander: {
+        token
+      },
+      success: (res) => {
+        if(res.data.errCode) { //有效则存档 globalData.token
+          this.globalData.token = token;
+        }else { //无效则重新登录
+          this.login();
         }
       }
     })
   },
-  globalData: {
-    userInfo: null
+  // 登录操作
+  login() {
+    console.log('请登录');
+    wx.login({
+      //code只有5分钟的有效期
+      success: (res) => {
+        //1.获取code
+        const code = res.code;
+        //2.将code发送给我们的服务器
+        wx.request({
+          url: 'http://httpbin.org/post',
+          method: 'POST',
+          data: {
+            code: code
+          },
+          success: (res) => {
+            res = {data: {token: 'st$90_dfjbFtty'}} //假设有数据
+            //1.取出koten
+            const token = res.data.token;
+            //2.将token保存在globalData中
+            this.globalData.token = token;
+            //3.尽心本地存储
+            wx.setStorageSync(TOKEN, token)
+          }
+        })
+      }
+    })
   }
 })
